2f73e9dc2f5c3be3d8eee74b614683be3325d9ca
Keiho Sakapon <git@saka-pon.net>
2019-02-21 23:39:08 +0900

    Add AssemblyInfo

---
+++ Speedrunner/Speedrunner.Core/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Runtime.CompilerServices;
+using System.Windows.Markup;
+
+[assembly: InternalsVisibleTo("UnitTest")]
+[assembly: XmlnsDefinition("http://schemas.saka-pon.net/uiAutoRun", "TasSample.Models")]
+[assembly: XmlnsPrefix("http://schemas.saka-pon.net/uiAutoRun", "uar")]

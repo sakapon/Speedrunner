617aa7976b3d88c7bb65c9cd707bd5efdc8debe8
Keiho Sakapon <git@saka-pon.net>
2019-02-27 17:28:10 +0900

    Add test

---
+++ Speedrunner/UnitTest/Activities/ActivityTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xaml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Speedrunner.Activities;
+
+namespace UnitTest.Activities
+{
+    [TestClass]
+    public class ActivityTest
+    {
+        [TestMethod]
+        public void Save_Workflow()
+        {
+            var path = $@"..\..\..\Activities\XAMLs\{nameof(Save_Workflow)}.xaml";
+            var expected = File.ReadAllText(path);
+
+            var wf = new SequentialWorkflow { Name = "Test" };
+            wf.Activities.Add(new Delay { Timeout = 300 });
+            var forRange = new ForRange { StartNumber = 3, RepeatCount = 10 };
+            forRange.Activities.Add(new Delay { Timeout = 300 });
+            forRange.Activities.Add(new Delay { Timeout = 300 });
+            wf.Activities.Add(forRange);
+
+            Assert.AreEqual(expected, XamlServices.Save(wf));
+        }
+    }
+}

4841ea475f860025fe7ef1ade033226cedd9e3fa
Keiho Sakapon <git@saka-pon.net>
2019-02-28 18:46:56 +0900

    Add classes

---
+++ Speedrunner/Speedrunner.UI/UI/Activity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Speedrunner.UI
+{
+    public class Return : Control
+    {
+        static Return()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Return), new FrameworkPropertyMetadata(typeof(Return)));
+        }
+    }
+
+    public class Delay : Control
+    {
+        public static readonly DependencyProperty TimeoutProperty =
+            DependencyProperty.Register("Timeout", typeof(int), typeof(Delay), new PropertyMetadata(0));
+
+        [Category("Activity")]
+        public int Timeout
+        {
+            get { return (int)GetValue(TimeoutProperty); }
+            set { SetValue(TimeoutProperty, value); }
+        }
+
+        static Delay()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Delay), new FrameworkPropertyMetadata(typeof(Delay)));
+        }
+    }
+}

dda8a429b07aa9e99acac909393d6116a53603df
Keiho Sakapon <git@saka-pon.net>
2019-02-28 17:43:36 +0900

    Use UI namespace

---
+++ Speedrunner/Speedrunner/UI/Activity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Speedrunner.UI
+{
+    public class Delay : Control
+    {
+        public static readonly DependencyProperty TimeoutProperty =
+            DependencyProperty.Register("Timeout", typeof(int), typeof(Delay), new PropertyMetadata(0));
+
+        [Category("Activity")]
+        public int Timeout
+        {
+            get { return (int)GetValue(TimeoutProperty); }
+            set { SetValue(TimeoutProperty, value); }
+        }
+
+        static Delay()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Delay), new FrameworkPropertyMetadata(typeof(Delay)));
+        }
+    }
+}

0b47052907f3d2716f58dfe5f8a07a6dd0ba2c93
Keiho Sakapon <git@saka-pon.net>
2019-03-02 18:46:19 +0900

    Convert controls to models

--- Speedrunner/Speedrunner.UI/ActivityHelper.cs
+++ Speedrunner/Speedrunner.UI/ActivityHelper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -15,9 +17,33 @@ namespace Speedrunner
 
         public static Activities.Activity ToActivity(Control control)
         {
-            if (control.GetType().Namespace != typeof(UI.Constants).Namespace) throw new ArgumentException();
+            var uiType = control.GetType();
+            if (uiType.Namespace != typeof(UI.Constants).Namespace) return null;
 
-            throw new NotImplementedException();
+            var type = ActivityTypes[uiType.Name];
+            if (!type.IsSubclassOf(typeof(Activities.Activity))) return null;
+
+            var activity = (Activities.Activity)Activator.CreateInstance(type);
+
+            // Copies property values.
+            var properties = uiType.GetProperties()
+                .Where(p => p.GetCustomAttribute<CategoryAttribute>()?.Category == UI.Constants.CategoryName)
+                .ToDictionary(p => p.Name, p => p.GetValue(control));
+            foreach (var p in properties)
+                type.GetProperty(p.Key).SetValue(activity, p.Value);
+
+            // Adds children.
+            if ((control is ItemsControl ic) && !ic.Items.IsEmpty)
+            {
+                var activities = (ICollection<Activities.Activity>)type.GetProperty("Activities").GetValue(activity);
+                var children = ic.Items.OfType<Control>()
+                    .Select(ToActivity)
+                    .Where(a => a != null);
+                foreach (var child in children)
+                    activities.Add(child);
+            }
+
+            return activity;
         }
 
         public static UI.SequentialWorkflow FindSequentialWorkflow(DependencyObject obj)

bf2f031091dd34d7ac7ff39cda9735b2f0d1da32
Keiho Sakapon <git@saka-pon.net>
2019-03-12 14:54:18 +0900

    Implement executing expressions

--- Speedrunner/Speedrunner.Core/Activities/CodeActivities.cs
+++ Speedrunner/Speedrunner.Core/Activities/CodeActivities.cs
@@ -18,7 +18,39 @@ namespace Speedrunner.Activities
 
         public override void Execute(WorkflowContext context)
         {
-            throw new NotImplementedException();
+            var match = ExpressionPattern.Match(Text);
+            if (!match.Success) throw new FormatException();
+
+            var leftVarName = match.Groups[1].Value;
+            var isLeftVarContained = context.Variables.Contains(leftVarName);
+
+            var leftVarIndex = isLeftVarContained ? Array.IndexOf(context.Variables.Dictionary.Keys.ToArray(), leftVarName) : context.Variables.Count;
+
+            var script_vars = string.Join(", ", context.Variables.Select(v => $"{(v.VariableName == leftVarName ? "ref " : "")}{v.Type.FullName} {v.VariableName}"));
+            if (!isLeftVarContained) script_vars += $", ref object {leftVarName}";
+            var source = $@"
+public static class Program
+{{
+public static void Execute({script_vars})
+{{
+{Text};
+}}
+}}";
+            var options = new CompilerParameters(new[] { "System.dll", "System.Core.dll" })
+            {
+                GenerateInMemory = true,
+            };
+            var provider = CodeDomProvider.CreateProvider("CSharp");
+            var results = provider.CompileAssemblyFromSource(options, source);
+            if (results.Errors.HasErrors) throw new FormatException();
+
+            var action = results.CompiledAssembly.GetType("Program").GetMethod("Execute");
+            var args = context.Variables.Select(v => v.ActualValue).ToArray();
+            if (!isLeftVarContained) args = args.Concat(new object[] { null }).ToArray();
+            action.Invoke(null, args);
+
+            var leftVar = context.Variables.Get(leftVarName, args[leftVarIndex].GetType());
+            leftVar.ActualValue = args[leftVarIndex];
         }
     }
 }

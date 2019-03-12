3bff9d54fe767aaeccc800de26117a1ddb6d6d1b
Keiho Sakapon <git@saka-pon.net>
2019-03-12 22:30:17 +0900

    v2.0.2

--- Speedrunner/Speedrunner.UI/Properties/AssemblyInfo.cs
+++ Speedrunner/Speedrunner.UI/Properties/AssemblyInfo.cs
@@ -52,8 +52,8 @@ using System.Windows.Markup;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1")]
+[assembly: AssemblyVersion("2.0.2.0")]
+[assembly: AssemblyFileVersion("2.0.2")]
 
 [assembly: XmlnsDefinition("http://schemas.saka-pon.net/speedrunner/ui", "Speedrunner.UI")]
 [assembly: XmlnsPrefix("http://schemas.saka-pon.net/speedrunner/ui", "sr")]

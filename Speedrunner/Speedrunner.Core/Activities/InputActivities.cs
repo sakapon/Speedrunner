42deff5871da52c0802e3491dfb35763440e81b3
Keiho Sakapon <git@saka-pon.net>
2019-03-17 20:18:12 +0900

    Add classes for click

---
+++ Speedrunner/Speedrunner.Core/Activities/InputActivities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using KLibrary.Labs.UI.Input;
+
+namespace Speedrunner.Activities
+{
+    public class Click : Delay
+    {
+        [DefaultValue("0,0")]
+        public Point Position { get; set; }
+
+        [DefaultValue(false)]
+        public bool UsesCurrentPoint { get; set; }
+
+        protected override void ExecuteAfterDelay()
+        {
+            if (UsesCurrentPoint)
+                MouseInjection.Click();
+            else
+                MouseInjection.Click(Position);
+        }
+    }
+}
---
+++ Speedrunner/Speedrunner.UI/UI/InputActivities.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+
+namespace Speedrunner.UI
+{
+    public class Click : Delay
+    {
+        public static readonly DependencyProperty PositionProperty =
+            DependencyProperty.Register("Position", typeof(Point), typeof(Click), new PropertyMetadata(new Point()));
+
+        public static readonly DependencyProperty UsesCurrentPointProperty =
+            DependencyProperty.Register("UsesCurrentPoint", typeof(bool), typeof(Click), new PropertyMetadata(false));
+
+        [Category(Constants.CategoryName)]
+        public Point Position
+        {
+            get { return (Point)GetValue(PositionProperty); }
+            set { SetValue(PositionProperty, value); }
+        }
+
+        [Category(Constants.CategoryName)]
+        public bool UsesCurrentPoint
+        {
+            get { return (bool)GetValue(UsesCurrentPointProperty); }
+            set { SetValue(UsesCurrentPointProperty, value); }
+        }
+
+        static Click()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Click), new FrameworkPropertyMetadata(typeof(Click)));
+        }
+    }
+}
